a7dab21159d0b64927a5c24c3f8d4445bc660978
rubenleisfp <rubenleisfp@gmail.com>
2025-04-22 10:53:20 +0200

    Actividad 23

--- src/main/kotlin/practicas/practica23/TareasViewModel.kt
+++ src/main/kotlin/practicas/practica23/TareasViewModel.kt
@@ -7,70 +7,18 @@ class TareasViewModel {
         get() = _uiState
 
     fun agregarTarea(tarea: Tarea) {
-        /* VERSION CON LAMBDAS
-        val nuevasTareas = _uiState.tareas + tarea
-        val nuevasPendientes = nuevasTareas.count { !it.completada }
-        _uiState = _uiState.copy(tareas = nuevasTareas, pendientes = nuevasPendientes)
-         */
-
-        val nuevasTareas = mutableListOf<Tarea>()
-        for (t in _uiState.tareas) {
-            nuevasTareas.add(t)
-        }
-        nuevasTareas.add(tarea)
-
-        val nuevasPendientes = contarPendientes(nuevasTareas)
-        _uiState = _uiState.copy(tareas = nuevasTareas.toList(), pendientes = nuevasPendientes)
+        throw UnsupportedOperationException("A implementar por el alumno")
     }
 
     fun completarTarea(id: Int) {
-        /**
-         VERSION CON LAMBDAS
-        val nuevasTareas = _uiState.tareas.map {
-            if (it.id == id) it.copy(completada = true) else it
-        }
-        val nuevasPendientes = nuevasTareas.count { !it.completada }
-        _uiState = _uiState.copy(tareas = nuevasTareas, pendientes = nuevasPendientes)
-        **/
-
-        val nuevasTareas = mutableListOf<Tarea>()
-        for (t in _uiState.tareas) {
-            if (t.id == id) {
-                nuevasTareas.add(t.copy(completada = true))
-            } else {
-                nuevasTareas.add(t)
-            }
-        }
-
-        val nuevasPendientes = contarPendientes(nuevasTareas)
-        _uiState = _uiState.copy(tareas = nuevasTareas.toList(), pendientes = nuevasPendientes)
+        throw UnsupportedOperationException("A implementar por el alumno")
     }
 
     fun eliminarTarea(id: Int) {
-
-        /* VERSION CON LAMBDAS
-        val nuevasTareas = _uiState.tareas.filterNot { it.id == id }
-        val nuevasPendientes = nuevasTareas.count { !it.completada }
-        _uiState = _uiState.copy(tareas = nuevasTareas, pendientes = nuevasPendientes)
-        */
-        val nuevasTareas = mutableListOf<Tarea>()
-        for (t in _uiState.tareas) {
-            if (t.id != id) {
-                nuevasTareas.add(t)
-            }
-        }
-        val nuevasPendientes = contarPendientes(nuevasTareas)
-        _uiState = _uiState.copy(tareas = nuevasTareas.toList(), pendientes = nuevasPendientes)
-
+        throw UnsupportedOperationException("A implementar por el alumno")
     }
 
     private fun contarPendientes(tareas: List<Tarea>): Int {
-        var contador = 0
-        for (t in tareas) {
-            if (!t.completada) {
-                contador++
-            }
-        }
-        return contador
+        throw UnsupportedOperationException("A implementar por el alumno")
     }
 }
