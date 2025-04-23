c2d5cbad3d3add70bd60b043127c9d0fe8831ca2
rubenleisfp <147842309+rubenleisfp@users.noreply.github.com>
2025-04-23 08:48:07 +0200

    Update UsuarioViewModel.kt

--- src/main/kotlin/practicas/practica21/UsuarioViewModel.kt
+++ src/main/kotlin/practicas/practica21/UsuarioViewModel.kt
@@ -8,6 +8,13 @@ class UsuarioViewModel {
     }
 
     fun cambiarEstadoUsuario(id: Int) {
+        //Crear una nueva lista a partir de la existente. 
+        //Recorremos la actual lista de usuarios y:
+        //A) si coincide el id del usuario con el recibido como argumento, cambiamos el estado del usuario
+        //y lo añadimos a la nueva lista. 
+        //B) Sino coincide simplemente deberemos incluir el usuario en la nueva lista
+        //Una vez que hayamos recorrido la lista de usuarios y volcado la información en la nueva lista, solo debemos sustituir
+        //nuestra lista de usuarios actual por la nueva.
         throw UnsupportedOperationException("A implementar por el alumno")
     }
 
