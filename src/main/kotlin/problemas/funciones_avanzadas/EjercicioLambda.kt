3e10ba6233b04dda07dd5c3b82409a9d93ec42b9
Ruben Leis <rubenleisfp@gmail.com>
2025-09-10 13:27:12 +0200

    Corregido EjercicioLambda

--- src/main/kotlin/problemas/funciones_avanzadas/EjercicioLambda.kt
+++ src/main/kotlin/problemas/funciones_avanzadas/EjercicioLambda.kt
@@ -1,17 +1,48 @@
-fun operar(v1: Int, v2: Int, fn: (Int, Int) -> Int) : Int{
+fun operar(v1: Int, v2: Int, fn: (Int, Int) -> Int) : Int {
     return fn(v1, v2)
 }
 
+
+fun sumar(x1: Int, x2: Int) = x1 + x2
+
+
+fun restar(x1: Int, x2: Int) = x1 - x2
+
+
+fun multiplicar(x1: Int, x2: Int) = x1 * x2
+
+
+fun dividir(x1: Int, x2: Int) = x1 / x2
+
+
 fun main() {
-    val suma = operar(2, 3, {x, y -> x + y})
-    println(suma)
-    val resta = operar(12, 2, {x, y -> x - y})
-    println(resta)
-    var elevarCuarta = operar(2, 4, {x, y ->
-        var valor = 1
-        for(i in 1..y)
-            valor = valor * x
-        valor
-    })
-    println(elevarCuarta)
+    //Ejemplo hecho para la suma. Funcion anonima
+    var resu1 = operar(10, 5, ::sumar)
+    println("La suma de 10 y 5 es $resu1")
+    //Ejemplo hecho para la suma. Expresión lambda
+    resu1 = operar(10, 5, {x, y -> x + y})
+    println("La suma de 10 y 5 es $resu1")
+
+    //RESTA
+    resu1 = operar(10, 5, ::restar)
+    println("La resta de 10 y 5 es $resu1")
+    //Ejemplo hecho para la suma. Expresión lambda
+    resu1 = operar(10, 5, {x, y -> x - y})
+    println("La resta de 10 y 5 es $resu1")
+
+    //MULTIPLICACION
+    resu1 = operar(10, 5, ::multiplicar)
+    println("La multiplicacion de 10 y 5 es $resu1")
+    //Ejemplo hecho para la multiplicacion. Expresión lambda
+    resu1 = operar(10, 5, {x, y -> x * y})
+    println("La multiplicacion de 10 y 5 es $resu1")
+
+    //DIVISION
+    resu1 = operar(10, 5, ::dividir)
+    println("La division de 10 entre 5 es $resu1")
+    //Ejemplo hecho para la division. Expresión lambda
+    resu1 = operar(10, 5, {x, y -> x / y})
+    println("La division de 10 entre 5 es $resu1")
+
+
 }
